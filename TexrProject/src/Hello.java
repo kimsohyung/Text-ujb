0e6f77c4662172c303227333e2b59a340dcc78fe
21-manic-013g <21-manic-013g@21-manic-013>
2023-09-20 16:34:42 +0900

    s

---
+++ TexrProject/src/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello {
+		public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("x값 입력:");
+			String strX = scanner.nextLine();
+			int x = Integer.parseInt(strX);
+}
+}
